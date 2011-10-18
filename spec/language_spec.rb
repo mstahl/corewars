2d6108a845053f796d4a54e7a36cc5d952d71af1
max thom stahl <max@dangerouscuteness.com>
2011-10-17 21:34:06 -0500

    Still trying to simplify the grammar enough that it, you know, actually works.

--- spec/language_spec.rb
+++ spec/language_spec.rb
@@ -1,31 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-# Imp from http://users.obs.carnegiescience.edu/birk/COREWAR/88/HILL/imp.red
-@@imp = <<__END_IMP__
-;redcode
-;name Imp
-;author A.K. Dewdney
-;assert 1
-
-imp     mov imp, imp + 1
-
-        end imp
-__END_IMP__
-
-@@dwarf = <<__END_DWARF__
-;redcode verbose
-;name Dwarf
-;author A. K. Dewdney
-;strategy Throw DAT bombs around memory, hitting every 4th memory cell.
-;strategy   This program was presented in the first Corewar article.
-;assert (CORESIZE % 4)==0
-bomb  dat #0
-dwarf add #4, bomb
-      mov bomb, @bomb
-      jmp dwarf
-      end dwarf
-__END_DWARF__
-
 describe "Redcode Language" do
   context "Parsing" do
     
@@ -45,7 +19,8 @@ __END_COMMENTARY__
     
     it 'can parse the simple program' do
       simple_text = <<__END_SIMPLE__
-org balls
+org foo
+mov 0,1
 __END_SIMPLE__
       simple = Warrior.new simple_text.strip
       simple.should_not be_nil
