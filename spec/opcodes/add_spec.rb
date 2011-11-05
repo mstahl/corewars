e55dff327a9a755aefeef1eb37e252862fca8efa
max thom stahl <max@dangerouscuteness.com>
2011-11-05 15:36:27 -0500

    Created a spec file for each opcode.

--- spec/opcodes/add_spec.rb
+++ spec/opcodes/add_spec.rb
@@ -1,7 +1,12 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 require 'pp'
 
 describe "The ADD instruction" do
-
+  before(:each) do
+    @core = Mars.new
+  end
+  
+  it 'should add two numbers in DAT '
+  
 end
--- spec/opcodes/dat_spec.rb
+++ spec/opcodes/dat_spec.rb
@@ -1,7 +1,12 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 require 'pp'
 
 describe "The DAT instruction" do
-
+  before(:all) do
+    @core = Mars.new
+  end
+  
+  
+  
 end
