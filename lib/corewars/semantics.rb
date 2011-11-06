2c27fdea6cf2e27700835f546ecf1a7398ec91c4
max thom stahl <max@dangerouscuteness.com>
2011-11-06 15:12:28 -0600

    Separated out some more code from corewars.rb

---
+++ lib/corewars/semantics.rb
@@ -0,0 +1,39 @@
+
+class Instruction < Treetop::Runtime::SyntaxNode
+  def value
+    hash = {
+      :opcode   => operation.value[:opcode],
+      :modifier => operation.value[:modifier],
+      :label    => lbl ? lbl.text_value.gsub(/\s+/, '_').gsub(/[^\w_]/, '').to_sym : nil
+    }
+    
+    hash[:a_mode] = a.value[:mode]
+    hash[:a] = a.value[:expression]
+    
+    
+    if defined? b
+      hash[:b_mode] = b.value[:mode]
+      hash[:b] = b.value[:expression]
+    end
+    
+    hash
+  end
+end
+
+class Operation < Treetop::Runtime::SyntaxNode
+  def value
+    {
+      :opcode   => opcode.text_value.to_sym,
+      :modifier => defined? modifier ? nil : modifier.text_value.to_sym
+    }
+  end
+end
+
+class Operand < Treetop::Runtime::SyntaxNode
+  def value
+    {
+      :expression => primary.value,
+      :mode       => defined? mode ? mode.text_value : nil
+    }
+  end
+end
---
+++ lib/corewars/warrior.rb
@@ -0,0 +1,91 @@
+
+
+class Warrior
+  attr_reader :instructions
+  attr_reader :labels
+  
+  attr_reader :metadata
+  
+  attr_reader :org
+  attr_reader :end
+  
+  attr_writer :start_address
+  
+  def initialize(text)
+    @instructions = []
+    @labels = {}
+    @metadata = {}
+    
+    text.lines.each_with_index do |line, line_no|
+      # Check for metadata
+      if line =~ /;@(\w+)\s+(.+)/ then
+        @metadata[$1.to_sym] ||= []
+        @metadata[$1.to_sym] << $2
+        next
+      end
+      # Compress whitespace
+      line.gsub! /\s+/, ' '
+      # Strip out the comments from the line
+      line.gsub! /;.*/, ''
+      line.strip!
+      # Attempt to parse
+      unless line.blank?
+        instruction = Mars.parse(line)
+        
+        @instructions << instruction
+      end
+    end
+    
+    # Before labels, we need to get rid of the 'org' and 'end' instructions, 
+    # but hang on to the data they contain.
+    @instructions.each_with_index do |instruction, address|
+      if instruction.value[:opcode] == :org then
+        @org = instruction.value[:a]
+        @instructions.delete instruction
+      elsif instruction.value[:opcode] == :end then
+        @end = address
+        @instructions.delete instruction
+      end
+    end
+    
+    # Should do something with labels here....
+    @instructions.each_with_index do |instruction, address|
+      next unless instruction.value[:label] =~ /^[\w\d_\s]+$/
+      
+      if instruction.value[:label] then
+        @labels[instruction.value[:label].to_sym] = address   # Label addresses are stored here relative to start of program
+      end
+    end
+    
+    if @org.nil? then
+      @org = 0
+    else
+      @org = @labels[@org]
+    end
+    
+    # The metadata are stored as an array of strings right now. They should
+    # be handled here to be human-readable.
+    if @metadata[:author] then
+      @metadata[:author] = @metadata[:author].join(',')
+    end
+    if @metadata[:strategy] then
+      @metadata[:strategy] = @metadata[:strategy].join(' ')
+    end
+    if @metadata[:name] then
+      @metadata[:name] = @metadata[:name][0]
+    end
+    
+  end
+  
+  def placed_at(address)
+    @start_address = address
+    
+    @org = @start_address + @org
+    
+    @labels.each do |lbl, addr|
+      @labels[lbl] = @start_address + addr
+    end
+    
+  end
+  
+end
