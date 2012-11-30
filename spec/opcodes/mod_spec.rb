7a6a34811161e52851c4dd149f3c800be5915738
max thom stahl <max@villainousindustries.com>
2012-11-29 21:01:12 -0600

    Fixed MOD

--- spec/opcodes/mod_spec.rb
+++ spec/opcodes/mod_spec.rb
@@ -11,14 +11,14 @@ describe "The MOD instruction" do
     warrior = Warrior.new %q{
                org warrior
       warrior: mod 1, 2
-               dat #13, #35
                dat #5, #6
+               dat #13, #35
     }
     @core.register_warrior warrior
     @core.step
 
-    @core[warrior.org + 1][:a].should == 13
-    @core[warrior.org + 1][:b].should == 35
+    @core[warrior.org + 1][:a].should == 5
+    @core[warrior.org + 1][:b].should == 6
     @core[warrior.org + 2][:a].should == 3
     @core[warrior.org + 2][:b].should == 5
   end
